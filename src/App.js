f9cd3e74de0867eb7640a07ede282ba3cb996fe8
Rohit Kumar <rohit952513@gmail.com>
2022-03-20 15:45:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import React from 'react'
+import {useState, useRef ,useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  let initialTodo;
+  if (localStorage.getItem("todos") === null) {
+    initialTodo = [];
+  }
+  else {
+    initialTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const [todoItems, setTodoItems] = useState(initialTodo);
+  const [currTask, setCurrTask] = useState("");
+
+  const inputTask = useRef(null); 
+
+  const AddToDo = () => {
+    if (!currTask) {
+      alert("Please!! Enter Task ✍");
+    }
+    else {
+      setTodoItems([...todoItems, {ToDo: currTask, status: false}]);
+      inputTask.current.value = "";
+      setCurrTask("");
+    }
+  }
+
+  const Delete = (val) => {
+    setTodoItems(
+      todoItems.filter((item, key) => {
+        return key !== val;
+      })
+    );
+    localStorage.setItem("todos", JSON.stringify(todoItems));
+  }
+
+  const Complete = (val) => {
+    setTodoItems(
+      todoItems.map((item, key) => {
+        return (key === val ? {ToDo: item.ToDo, status: true} : {ToDo: item.ToDo, status: item.status})
+      })
+    )
+  }
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todoItems));
+  }, [todoItems])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section id="Input">
+        <h1>📃 To-Do List ✍</h1>
+        <div>
+          <input 
+            ref={inputTask}
+            type="text" 
+            placeholder="📃Enter a Task..."
+            onChange={(e) => {setCurrTask(e.target.value)}} 
+            onKeyDown={(e) => {if(e.code === 'Enter') AddToDo();}} 
+          />
+          <button id="Add" onClick={AddToDo}>✍Add Task</button>
+        </div>
+      </section>
+      <hr/>
+      <section id="Todos">
+        <ul>
+          {todoItems.length===0?<li>👌 Hurrah!! No Task To Do 🏆</li>:
+            todoItems.map((item, key) => {
+              return (
+                <div id="ToDo">
+                  <li key={key}>{item.ToDo}</li>
+                  <button id="comp" onClick={() => {Complete(key)}}>🏆Completed</button>
+                  <button id="del" onClick={() => {Delete(key)}}>❌Delete!!</button>
+                  {item.status ? <h3>Status : 👍 Done!!</h3> : <h3>Status : Not Done!!</h3> }
+                </div>
+                
+              )
+            })
+          }
+        </ul>
+      </section>
     </div>
   );
 }
 
 export default App;
+
